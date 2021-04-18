b25c1fb3d0d8a2bc32a4e456892b15660252e2df
Samuel Lin <samuel990705@gmail.com>
2021-04-18 12:57:56 +0800

    Used new asset for player.

--- Assets/CharacterControllerMove.cs
+++ Assets/CharacterControllerMove.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class CharacterControllerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    CharacterController controller;
+    Animator animator;
+
+    float speed = 2.0f;
+    float jumpSpeed = 8.0f;
+    float gravity = 20.0f;
+
+    Vector3 moveDirection = Vector3.zero;
+    float xMove;
+    float yMove;
+    bool jump;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        animator.SetBool("Grounded", controller.isGrounded);
+
+        if (controller.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+            moveDirection = new Vector3(xMove, 0.0f, yMove);
+            moveDirection *= speed;
+
+            // Face in direction of movement.
+            if (moveDirection.magnitude > float.Epsilon)
+            {
+                transform.rotation = Quaternion.LookRotation(moveDirection);
+            }
+
+            if (jump)
+            {
+                moveDirection.y = jumpSpeed;
+            }
+            
+            
+        }
+        //set moveSpeed before applying gravity
+        animator.SetFloat("MoveSpeed", moveDirection.magnitude);
+
+        moveDirection.y -= gravity * Time.fixedDeltaTime;
+
+        Debug.Log(animator.GetFloat("MoveSpeed"));
+        controller.Move(moveDirection * Time.fixedDeltaTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        xMove = Input.GetAxis("Horizontal");
+        yMove = Input.GetAxis("Vertical");
+
+        jump = Input.GetButton("Jump");
+
     }
 }
