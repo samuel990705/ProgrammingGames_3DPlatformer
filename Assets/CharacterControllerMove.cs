85e03b654ba4c56c6b5d7ea7f81eb2643a7a2946
Samuel Lin <samuel990705@gmail.com>
2021-04-19 00:17:46 +0800

    Added campfire and interaction of player with campfire

--- Assets/CharacterControllerMove.cs
+++ Assets/CharacterControllerMove.cs
@@ -20,9 +20,19 @@ public class CharacterControllerMove : MonoBehaviour
     float yMove;
     bool jump;
 
+    public bool onFire;//used to interact with campfire
+    [SerializeField]GameObject fire;//particle effect used to spawn fire on head lol
+    GameObject flame;//actual flame prefab that is spawned ontop of Player
+    float flameOffset=1.3f;//y-offset of where the flame will be
+    int burnTimer;//used to keep track of how long player is burning
+    float burningSpeed=10f;//speed of player when player is on fire
+    Vector3 burningMove;//equivalent to moveDirection but for when player is on fire
+
     //allows for some steering while player is in-air for a more fluid motion
     Vector3 jumpDirection;
     private float strafeSpeed = 5.0f;//speed of player in the air
+
+
 
     void Start()
     {
@@ -32,17 +42,73 @@ public class CharacterControllerMove : MonoBehaviour
         xMove = 0f;
         yMove = 0f;
         groundedBuffer = 0;
+        burnTimer = -1;
+        burningMove = new Vector3();
     }
 
     private void FixedUpdate()
     {
+        if (onFire)//if player is currently on fire
+        {
+            animator.SetBool("Grounded", controller.isGrounded);//always running when on fire
+            if (flame==null)//just caught on fire
+            {
+                //instantiate flame prefab
+                flame = Instantiate(fire, new Vector3(this.transform.position.x, this.transform.position.y + flameOffset, this.transform.position.z), Quaternion.identity);
+                burnTimer = 50 * 4;//FixedUpdate is called 50times per second, therefore burns for 4s
+            }
+            //makes flame follow Player
+            flame.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + flameOffset, this.transform.position.z);
 
-        animator.SetBool("Grounded", controller.isGrounded);
-        groundedBuffer += Time.fixedDeltaTime;
+            if (burnTimer <= 0)//when timer runs out
+            {
+                onFire = false;//reset variables
+                burnTimer = 0;
+                Destroy(flame);
+            }
+            if (burnTimer % 60 == 0)//every 60 frames (3 times in 200 frames)
+            {
+                Debug.Log(burnTimer);
+
+                do//don't allow both x,y to be 0, or else player will just stand still
+                {
+                    burningMove.x = Random.Range(-1, 2);//go in random direction (every 60 frames)
+                    burningMove.z = Random.Range(-1, 2);//random either -1, 0, or 1
+                } while (burningMove.x == 0 && burningMove.z == 0);
+            }
+            if (burnTimer % 50 == 0)//make player jump every 2 seconds
+            {
+                burningMove.y = jumpSpeed*0.7f;//make character jump
+            }
+
+            //move player
+            animator.SetFloat("RunAnimationSpeed", 5f);//speed up running animation speed
+
+            // Face in direction of movement.
+            if (Mathf.Abs(burningMove.x) > float.Epsilon || Mathf.Abs(burningMove.z) > float.Epsilon)
+            {
+                transform.rotation = Quaternion.LookRotation(new Vector3(burningMove.x, 0f, burningMove.z));
+            }
+
+            //gravity
+            burningMove.y -= gravity * Time.fixedDeltaTime;
+
+            controller.Move(burningMove * burningSpeed * Time.fixedDeltaTime);
+
+
+            burnTimer--;//decremenet burnTimer
+            return;//skip everything below
+        }
+
+        animator.SetFloat("RunAnimationSpeed", 1f);
+
+
+        animator.SetBool("Grounded", controller.isGrounded);//updated animation parameter
+        groundedBuffer += Time.fixedDeltaTime;//keeps track of time player is not grounded
         if (controller.isGrounded)
         {
-            groundedBuffer = 0;
-            // We are grounded, so recalculate
+            groundedBuffer = 0;//set buffer to 0 if player is grounded
+
             // move direction directly from axes
             moveDirection = new Vector3(xMove, 0.0f, yMove);
             moveDirection *= speed;
@@ -50,7 +116,7 @@ public class CharacterControllerMove : MonoBehaviour
             //set moveSpeed before appending y-movement
             animator.SetFloat("MoveSpeed", moveDirection.magnitude);
 
-            if (jump)
+            if (jump)//if player jumps
             {
                 jumpDirection = 1/speed*moveDirection;//record direction when play jumps
                 moveDirection.y = jumpSpeed;
@@ -64,7 +130,7 @@ public class CharacterControllerMove : MonoBehaviour
             moveDirection = new Vector3(speed*jumpDirection.x+(xMove-jumpDirection.x)*strafeSpeed, moveDirection.y, speed * jumpDirection.z + (yMove-jumpDirection.z) * strafeSpeed);
         }
 
-        animator.SetFloat("GroundedBuffer", groundedBuffer);
+        animator.SetFloat("GroundedBuffer", groundedBuffer);//update animation parameter
 
         // Face in direction of movement.
         if (Mathf.Abs(moveDirection.x) > float.Epsilon || Mathf.Abs(moveDirection.z) > float.Epsilon)
@@ -72,9 +138,10 @@ public class CharacterControllerMove : MonoBehaviour
             transform.rotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
         }
 
+        //implement gravity
         moveDirection.y -= gravity * Time.fixedDeltaTime;
 
-        
+        //move character in moveDirection
         controller.Move(moveDirection * Time.fixedDeltaTime);
     }
 
@@ -86,5 +153,10 @@ public class CharacterControllerMove : MonoBehaviour
 
         jump = Input.GetButton("Jump");
 
+
+        if (this.transform.position.y <=-4f)//player have fallen off the map
+        {
+            this.transform.position = new Vector3(1.805f, 4f, -6.66f);//return to spawn
+        }
     }
 }
---
+++ Assets/Fire.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Collide");
+            CharacterControllerMove c = collision.GetComponent<CharacterControllerMove>();
+            if (!c.onFire)//if not already on fire
+                c.onFire=true;//set player onFire to true
+        }
+    }
+}
